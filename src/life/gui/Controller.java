2ad18e8440f4c8981591c745cb21cb6b753c6a49
valexby <realnats@gmail.com>
2016-03-28 08:49:37 +0300

    I't my first commit. It pretty large. Next commits will be smaller. There is stabale version of life game.
    Change-Id: I1197176007d755baa58a3af4090a7a99c31cdb17
    
    Tryint to make sizeble game field.
    
    Change-Id: I0b27563cfc33fbeb80f7a395f455c52083dc8a21
    
    Resizable version. TextFields replaced by Sliders.
    
    Change-Id: I95db1ab645f2f67ac766386a9d557941f3e21135
    
    Speed change during animation added
    
    Change-Id: If15a8ab9bb70a07cbea020111d65bdf43d2fee51
    
    Infinit life, and clean button added
    
    Change-Id: Ib7aab5c16902728b37d0e85abd278b8f16866d77
    
    Foreach replaced by streams with lambda
    
    Change-Id: I35d42d24062d8c2febaf1460c47213fa14ce0389
    
    Resize attach by lambda
    
    Change-Id: I9dcc9b133ccab2f7f164fd4b15b2f8c9aabe6089
    
    first test
    
    Change-Id: I5e8f1323f556201ccfda2537522566071fdcd08a
    
    second test
    
    Change-Id: I2fc115c86daf8289fb6f8a1050b90cb650fa41fd

---
+++ src/life/gui/Controller.java
@@ -0,0 +1,147 @@
+package life.gui;
+
+/**
+ * Created by valex on 23.3.16.
+ */
+
+import java.beans.Expression;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.util.Duration;
+import life.core.Board;
+
+public class Controller implements Initializable {
+
+    final static private int horizontalBorderSz = 243, verticalBorderSz = 15, cellSpacePx = 35, cellSizePx = 30,
+            maxFrequency = 300;
+
+    @FXML
+    private FlowPane base;
+    @FXML
+    private Button runButton, stopButton;
+
+    @FXML
+    private HBox rootBox;
+
+    @FXML
+    private Slider densitySlider, freqSlider;
+
+    private Board board;
+
+    private DisplayDriver display;
+
+    private Timeline loop = null;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        board = new Board();
+        createDisplay();
+        attachResizeListeners();
+    }
+
+    @FXML
+    private void onRun(Event evt) {
+        toggleButtons(false);
+        loopStart(null);
+    }
+
+    @FXML
+    private void onStop(Event evt) {
+        toggleButtons(true);
+        loop.stop();
+    }
+
+    @FXML
+    private void onGenerate(Event evt) {
+        board.generate(densitySlider.getValue());
+        display.displayBoard(board);
+    }
+
+    @FXML
+    private void onFreqChanged(Event evt) {
+        if (loop.getStatus() == Animation.Status.RUNNING)
+            loopStart(loop);
+    }
+
+    @FXML
+    private void onClean(Event evt) {
+        if (loop != null && loop.getStatus() == Animation.Status.RUNNING)
+            onStop(evt);
+        board.resetGrid();
+        display.displayBoard(board);
+    }
+
+    private void toggleButtons(boolean enable) {
+        runButton.setDisable(!enable);
+
+        stopButton.setDisable(enable);
+    }
+
+    private void loopStart(Timeline oldLoop) {
+        loop = new Timeline(new KeyFrame(Duration.millis(maxFrequency / freqSlider.getValue()), event -> {
+            board.update();
+            display.displayBoard(board);
+        }));
+
+        loop.setCycleCount(Animation.INDEFINITE);
+        if (oldLoop != null) oldLoop.stop();
+        loop.play();
+    }
+
+    private void createDisplay() {
+        display = new DisplayDriver(cellSizePx, board);
+
+        base.getChildren().clear();
+        base.getChildren().add(new Group(display.getPane()));
+    }
+
+    @FunctionalInterface
+    private interface SizeSetter {
+        void resize(int size);
+    }
+
+    @FunctionalInterface
+    private interface SizeGetter {
+        int getSize();
+    }
+
+    private void resizeInterface(Number oldValue, Number newValue, int borderSize, SizeGetter getter, SizeSetter setter){
+        int newSize = newValue.intValue() - borderSize;
+        if (newSize > 0 && Math.abs(newSize / cellSpacePx - getter.getSize()) > 0) {
+            setter.resize(newSize / cellSpacePx);
+            display = new DisplayDriver(cellSizePx, board);
+
+            base.getChildren().clear();
+            base.getChildren().add(new Group(display.getPane()));
+        }
+    }
+
+    private void attachResizeListeners() {
+        rootBox.widthProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
+            SizeGetter getter = board::getCols;
+            SizeSetter setter = board::setCols;
+                resizeInterface(oldValue, newValue, horizontalBorderSz, getter, setter);
+        });
+        rootBox.heightProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
+            SizeGetter getter = board::getRows;
+            SizeSetter setter = board::setRows;
+            resizeInterface(oldValue, newValue, verticalBorderSz, getter, setter);
+        });
+    }
+    //test2
+}
